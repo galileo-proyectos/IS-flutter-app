7a9cfb3bbb865484bcb956b20fc899d32bc93019
subuyugaby <subuyu.gaby@gmail.com>
2023-10-13 00:49:59 -0600

    feat: home

--- lib/src/ui/MyRoutes.dart
+++ lib/src/ui/MyRoutes.dart
@@ -24,7 +24,7 @@ import 'package:provider/provider.dart';
 
 class MyRoutes {
 
-  static const _initialLocation = '/';
+  static const _initialLocation = '/products';
 
   static final GoRouter _routes = GoRouter(
     initialLocation: _initialLocation,
