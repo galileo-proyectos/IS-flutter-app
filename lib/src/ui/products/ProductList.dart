738889a2c3a25428871fc711a62e9a9bac863078
subuyugaby <subuyu.gaby@gmail.com>
2023-10-13 20:04:09 -0600

    fix: home screen

--- lib/src/ui/products/ProductList.dart
+++ lib/src/ui/products/ProductList.dart
@@ -21,47 +21,48 @@ class _ScreenProductListState extends State<ScreenProductList> {
 
   @override
   Widget build(BuildContext ctx) {
-    return SingleChildScrollView(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Center(child: Text('ScreenProductList')),
-      Center(
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Center(child: Text('ScreenProductList')),
+        Consumer<ProviderProducts>(builder: (ctx, provider, child) {
+          if (provider.isLoading()) {
+            // here you can show a loading status
+            // CHANGE YOUR CODE HERE
+            return Text('loading...');
+          } else {
+            // here you can construct your category list
+            // to finished just return the widget u want to show
+            // CHANGE YOUR CODE HERE
+            List<Widget> listdewidgets = [];
 
-          // Because of consumer when new data is available
-          // the widget will automatically be refreshed.
-          // This piece is important, modify it but do not remove.
-          child: Consumer<ProviderProducts>(builder: (ctx, provider, child) {
-        if (provider.isLoading()) {
-          // here you can show a loading status
-          // CHANGE YOUR CODE HERE
-          return Text('loading...');
-        } else {
-          // here you can construct your category list
-          // to finished just return the widget u want to show
-          // CHANGE YOUR CODE HERE
-          List<Widget> listdewidgets = [];
+            // [pending]: how we suppose to handle thousands of products???
+            for (final product in provider.list) {
+              listdewidgets.add(WidgetProduct(
+                product: product,
+                onPressed: () {
+                  // CHANGE YOR CODE HERE
+                  // Alessandro implement this wierd approach thinking that a supermarket
+                  // will have thousands of products... this is faster :D
 
-          // [pending]: how we suppose to handle thousands of products???
-          for (final product in provider.list) {
-            listdewidgets.add(WidgetProduct(
-              product: product,
-              onPressed: () {
-                // CHANGE YOR CODE HERE
-                // Alessandro implement this wierd approach thinking that a supermarket
-                // will have thousands of products... this is faster :D
+                  // first you have to selected the product
+                  Provider.of<ProviderProducts>(context, listen: false)
+                      .selectProduct(product);
+                  // the you can show it's details.
 
-                // first you have to selected the product
-                Provider.of<ProviderProducts>(context, listen: false)
-                    .selectProduct(product);
-                // the you can show it's details.
+                  ctx.go('/products/details');
+                },
+              ));
+            }
 
-                ctx.go('/products/details');
-              },
-            ));
+            return Expanded(
+              child: GridView.count(
+                  childAspectRatio: 0.9,
+                  crossAxisCount: 2,
+                  children: listdewidgets),
+            );
           }
+        })
+      ]);
 
-          return Column(children: listdewidgets);
-        }
-      }))
-    ]));
+
   }
 }
