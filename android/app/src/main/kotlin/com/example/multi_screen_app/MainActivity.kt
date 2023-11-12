d9598d5f7ba9e6e49171450d6a124d26b8f4091a
Alessandro <alssdev3@gmail.com>
2023-11-11 21:24:47 -0600

    fix(themes): android themes fixed

--- android/app/src/main/kotlin/com/example/multi_screen_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/multi_screen_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.multi_screen_app
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
